4707fcaffbea4f144701241e232e077c87f790e9
rinashewale111 <rinashewale6@gmail.com>
2026-04-08 23:52:38 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const connectToDb=require("./Src/config/database")
 
 connectToDb()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on ${PORT}`);
